77b37d6fc488e1c6e463cd85f8dcf9d765bc07df
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2023-02-22 15:07:00 +0200

    charts now shows

--- src/app/pages/chart-page/chart-page.component.ts
+++ src/app/pages/chart-page/chart-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { BitcoinService } from 'src/app/services/bitcoin.service'
-import { Observable } from 'rxjs'
+import { Observable, from } from 'rxjs'
 import { Data } from 'src/app/model/chart'
 
 @Component({
@@ -13,7 +13,7 @@ export class ChartPageComponent implements OnInit {
   constructor(private bitcoinService: BitcoinService) { }
   prices$!: Observable<Data>
   ngOnInit(): void {
-      // this.prices$ = this.bitcoinService.getMarketPrice()
+      this.prices$ = from(this.bitcoinService.getMarketPrice())
   }
 
 }
